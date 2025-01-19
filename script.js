60afaaed23350f0d8d1641710bbce0ae4e6c0e7a
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 01:07:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hMzAiLCJhIjoiY201b3NkOG11MG9yYTJtcWF6c
 // 建立地圖實例
 const map = new mapboxgl.Map({
     container: 'map', // 目標容器元素的ID
-    style: 'mapbox://styles/mapbox/streets-v11', // 使用Mapbox的樣式
+    style: 'mapbox://styles/ryanma30/cm63unni9006n01st54922pqa', // 使用Mapbox的樣式
     center: [121.570, 25.052], // 設定初始地圖中心經緯度
     zoom: 12 // 設定初始縮放級別
 });
 
 // 可選：新增一個導航控制器
 map.addControl(new mapboxgl.NavigationControl());
+
+
