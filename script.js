a57a889b9c59f6574f7fcf985bbb31deae45c265
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 01:50:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/GeoJSONLayer"
-], function(Map, MapView, GeoJSONLayer) {
-    
-    // 創建 ArcGIS 地圖
-    var map = new Map({
-        basemap: "topo-vector" // 使用底圖
+    "esri/WebMap"
+], function(Map, MapView, WebMap) {
+    var webMap = new WebMap({
+        portalItem: { id: "ab90c3925fcb42378e4d23fff2eaee26" } // 替換為你的 Web Map ID
     });
 
-    // 創建 MapView
     var view = new MapView({
         container: "mapViewDiv",
-        map: map,
-        center: [121.5645, 25.0330], // 台北
-        zoom: 13
+        map: webMap
     });
-
-    // GeoJSONLayer 用來顯示 GeoJSON 數據
-    var geojsonLayer = new GeoJSONLayer({
-        url: "https://github.com/ryanma20/GeoRyanMa/blob/cd84c783c482f6711320a7513f973bbe389c474d/Neihu.geojson"
-    });
-
-    // 將 GeoJSONLayer 添加到地圖
-    map.addLayer(geojsonLayer);
 });
