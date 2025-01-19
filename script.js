98f6c8979a4486b85fc2bb80b573746bb4486841
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 01:19:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hMzAiLCJhIjoiY201b3NkOG11MG9yYTJtcWF6cmVua2xwZyJ9.MrpueCdCb70KgO23sbDwlQ';
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // 或者你可以使用自定義樣式
-    center: [121.5645, 25.0330], // 台北
+    style: 'mapbox://styles/ryanma30/cm63unni9006n01st54922pqa', // 或者你可以使用自定義樣式
+    center: [121.570 , 25.080], // 台北
     zoom: 13,
 });
 // 新增一個導航控制器 可選
