7e00b24cb82cbf02582ad53f327436003b84dd47
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-19 22:58:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hMzAiLCJhIjoiY201b3NkOG11MG9yYTJtcWF6c
 const map = new mapboxgl.Map({
     container: 'map', // 目標容器元素的ID
     style: 'mapbox://styles/mapbox/streets-v11', // 使用的 Mapbox 樣式
-    center: [121.5654, 25.0330], // 設定初始地圖中心經緯度
+    center: [121.571392, 25.078936], // 設定初始地圖中心經緯度
     zoom: 5 // 設定初始縮放級別
 });
 
