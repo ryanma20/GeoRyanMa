983442996498402936b59b824799ba62377e838a
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 01:30:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-// 設定 Mapbox Access Token
-mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hMzAiLCJhIjoiY201b3NkOG11MG9yYTJtcWF6cmVua2xwZyJ9.MrpueCdCb70KgO23sbDwlQ';
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/ryanma30/cm63unni9006n01st54922pqa', // 或者你可以使用自定義樣式
-    center: [121.570 , 25.080], // 台北
-    zoom: 13,
+require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/GeoJSONLayer"
+], function(Map, MapView, GeoJSONLayer) {
+    
+    // 創建 ArcGIS 地圖
+    var map = new Map({
+        basemap: "topo-vector" // 使用底圖
+    });
+
+    // 創建 MapView
+    var view = new MapView({
+        container: "mapViewDiv",
+        map: map,
+        center: [121.5645, 25.0330], // 台北
+        zoom: 13
+    });
+
+    // GeoJSONLayer 用來顯示 GeoJSON 數據
+    var geojsonLayer = new GeoJSONLayer({
+        url: "https://raw.githubusercontent.com/ryanma20/GeoRyanMa/fceaf56468533af1737f2b8a3e60f222dd6269bc/Neihui.geojson"
+    });
+
+    // 將 GeoJSONLayer 添加到地圖
+    map.addLayer(geojsonLayer);
 });
-// 新增一個導航控制器 可選
-map.addControl(new mapboxgl.NavigationControl());
