efcca331fc42de0c5bcfb59b2330bb54eb1c7ea8
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-19 22:47:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// 設定 Mapbox Access Token
+mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hMzAiLCJhIjoiY201b3NkOG11MG9yYTJtcWF6cmVua2xwZyJ9.MrpueCdCb70KgO23sbDwlQ';
+
+// 建立地圖實例
+const map = new mapboxgl.Map({
+    container: 'map', // 目標容器元素的ID
+    style: 'mapbox://styles/mapbox/streets-v11', // 使用的 Mapbox 樣式
+    center: [121.5654, 25.0330], // 設定初始地圖中心經緯度
+    zoom: 12 // 設定初始縮放級別
+});
+
+// 可選：新增一個導航控制器
+map.addControl(new mapboxgl.NavigationControl());
