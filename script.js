e8d6dcca8d08b95c5f17abe41ec393f4847b9a38
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 00:49:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hMzAiLCJhIjoiY201b3NkOG11MG9yYTJtcWF6c
 // 建立地圖實例
 const map = new mapboxgl.Map({
     container: 'map', // 目標容器元素的ID
-    style: 'mapbox://styles/ryanma30/cm5ospz7s00dj01rafumq3z7f', // 使用Mapbox的樣式
+    style: 'mapbox://styles/ryanma30/cm63unni9006n01st54922pqa', // 使用Mapbox的樣式
     center: [121.570, 25.052], // 設定初始地圖中心經緯度
     zoom: 12 // 設定初始縮放級別
 });
