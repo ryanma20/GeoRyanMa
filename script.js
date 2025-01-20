4269b60f20e06358504d53b1560186c82fcf3e1e
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 11:17:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/GeoJSONLayer"
-], function (Map, MapView, GeoJSONLayer) {
+    "esri/layers/GeoJSONLayer",
+    "esri/symbols/SimpleFillSymbol",
+    "esri/symbols/SimpleLineSymbol",
+    "esri/Color"
+], function(Map, MapView, GeoJSONLayer, SimpleFillSymbol, SimpleLineSymbol, Color) {
     // Initialize the map
 const map = new Map({
     basemap: "streets-navigation-vector"
 });
 
 const view = new MapView({
-    container: "viewDiv",
+    container: "mapViewDiv",
     map: map,
     center: [121.573, 25.076], // 設定中心點座標（內科）
     zoom: 12
