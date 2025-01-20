1a138289f0d34c81a7ee9ebc5f18588621059aff
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 15:59:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/GeoJSONLayer"
-], function(Map, MapView, GeoJSONLayer) {
+    "esri/layers/GeoJSONLayer",
+    "esri/widgets/Popup",
+    "esri/widgets/Legend"  // 載入 Legend 小工具
+], function(Map, MapView, GeoJSONLayer, Popup, Legend) {
 
     // 創建地圖
     var map = new Map({
@@ -11,13 +13,13 @@ require([
 
     // 創建 MapView（地圖視圖）
     var view = new MapView({
-        container: "viewDiv", // 指定地圖容器 ID
+        container: "mapViewDiv", // 指定地圖容器 ID
         map: map,
         center: [121.573, 25.076], // 地圖中心位置 (內科)
         zoom: 13 // 設定縮放級別
     });
-    
-    // 創建 GeoJSONLayer
+
+    // 創建 GeoJSONLayer (面資料)
     var geojsonLayer1 = new GeoJSONLayer({
         url: "https://raw.githubusercontent.com/ryanma20/GeoRyanMa/refs/heads/main/Geojsonfiles/Neihu.geojson",
         renderer: {
@@ -41,7 +43,7 @@ require([
             symbol: {
                 type: "simple-marker", // 簡單點符號
                 style: "circle",
-                color: [255,0, 0, 0.8], // 紅色
+                color: [0, 255, 0, 0.8], // 綠色
                 size: 8,
                 outline: {
                     color: [255, 255, 255],
@@ -56,7 +58,6 @@ require([
         title: "{name}", // 使用 {name} 顯示每個點的名稱
         content: function(graphic) {
             var name = graphic.attributes.name || "無名稱"; // 確保名稱存在
-            console.log("點的屬性:", graphic.attributes); // 檢查屬性
             return "點名稱: " + name;
         }
     };
@@ -101,4 +102,30 @@ require([
     // 將兩個 GeoJSONLayer 添加到地圖
     map.addMany([geojsonLayer1, geojsonLayer2]);
 
-});
+    // 為每個圖層添加加載成功/失敗的日誌
+    geojsonLayer1.when(() => {
+        console.log("面資料 GeoJSONLayer 加載成功");
+    }).catch((error) => {
+        console.error("面資料 GeoJSONLayer 加載失敗:", error);
+    });
+
+    geojsonLayer2.when(() => {
+        console.log("點資料 GeoJSONLayer 加載成功");
+    }).catch((error) => {
+        console.error("點資料 GeoJSONLayer 加載失敗:", error);
+    });
+
+    // 調整視圖範圍以顯示所有資料
+    Promise.all([
+        geojsonLayer1.when(),
+        geojsonLayer2.when()
+    ]).then(function() {
+        view.goTo({
+            target: [geojsonLayer1.fullExtent, geojsonLayer2.fullExtent],
+            padding: 50
+        }).catch(function(error) {
+            console.error("無法調整視圖範圍:", error);
+        });
+    }).catch(function(error) {
+        console.error("至少一個 GeoJSON 加載失敗:", error);
+    });
