ebea345f3bc9bed1fa4d1580a79df1d5200e253d
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 11:40:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 require([
-  "esri/Map",
-  "esri/views/MapView",
-  "esri/layers/GeoJSONLayer",
-  "esri/layers/TileLayer",
-  "esri/geometry/Extent",
-  "esri/Color"
-], function(Map, MapView, GeoJSONLayer, TileLayer, Extent, Color) {
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/GeoJSONLayer"
+], function(Map, MapView, GeoJSONLayer) {
 
-    // 建立地圖對象，使用 ArcGIS 提供的底圖
+    // 創建地圖
     var map = new Map({
-        basemap: "streets" // ArcGIS 提供的預設街道底圖
+        basemap: "topo-vector" // 使用底圖
     });
 
-    // 設置地圖視圖
+    // 創建 MapView（地圖視圖）
     var view = new MapView({
-        container: "mapViewDiv", // 地圖容器
+        container: "mapViewDiv", // 指定地圖容器 ID
         map: map,
         center: [121.076, 25.573], // 地圖中心位置 (內科)
-        zoom: 12 // 地圖縮放級別
+        zoom: 13 // 設定縮放級別
     });
 
-    // 讀取 Neihu.geojson 並將其添加為圖層
+    // 創建 GeoJSONLayer
     var geojsonLayer = new GeoJSONLayer({
-        url: 'https://raw.githubusercontent.com/ryanma20/GeoRyanMa/refs/heads/main/Geojsonfiles/Neihu.geojson'
+        url: "https://raw.githubusercontent.com/ryanma20/GeoRyanMa/refs/heads/main/Geojsonfiles/Neihu.geojson"
     });
 
-    // 將 GeoJSON 圖層添加到地圖
+    // 加入 GeoJSONLayer 到地圖
     map.add(geojsonLayer);
+
+    // 當 GeoJSON 加載完成時，調整視圖範圍
+    geojsonLayer.on("load", function() {
+        view.goTo({
+            target: geojsonLayer.fullExtent
+        });
+    });
 
 });
