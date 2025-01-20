a3f63398ade5893ff0a3ed9308ae8bb9dccacf6d
RyanMa <36875512+ryanma20@users.noreply.github.com>
2025-01-20 15:55:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,104 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hMzAiLCJhIjoiY202NHF4YzhlMWozbTJsc2ZocTY5Zml6ciJ9.itSMR7rZYezOyC9Mu8Qpcg'; // 使用你提供的 Mapbox token
+require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/GeoJSONLayer"
+], function(Map, MapView, GeoJSONLayer) {
 
-// 創建地圖
-var map = new mapboxgl.Map({
-    container: 'map', // 指定地圖容器 ID
-    style: 'mapbox://styles/mapbox/dark-v10', // 使用 Mapbox 的暗色風格
-    center: [121.573, 25.076], // 地圖中心位置 (內科)
-    zoom: 13 // 設定縮放級別
-});
-
-// 當地圖加載完成時，添加 GeoJSON 層
-map.on('load', function() {
-    // 添加 GeoJSON 層 (Neihu.geojson)
-    map.addSource('neihu', {
-        type: 'geojson',
-        data: 'https://raw.githubusercontent.com/ryanma20/GeoRyanMa/refs/heads/main/Geojsonfiles/Neihu.geojson'
+    // 創建地圖
+    var map = new Map({
+        basemap: "dark-gray" // 使用底圖
     });
 
-    map.addLayer({
-        id: 'neihu-layer',
-        type: 'fill',
-        source: 'neihu',
-        layout: {},
-        paint: {
-            'fill-color': 'rgba(0, 255, 255, 0.5)', // 半透明藍色
-            'fill-outline-color': 'rgba(0, 255, 255, 1)',
+    // 創建 MapView（地圖視圖）
+    var view = new MapView({
+        container: "viewDiv", // 指定地圖容器 ID
+        map: map,
+        center: [121.573, 25.076], // 地圖中心位置 (內科)
+        zoom: 13 // 設定縮放級別
+    });
+    
+    // 創建 GeoJSONLayer
+    var geojsonLayer1 = new GeoJSONLayer({
+        url: "https://raw.githubusercontent.com/ryanma20/GeoRyanMa/refs/heads/main/Geojsonfiles/Neihu.geojson",
+        renderer: {
+            type: "simple",
+            symbol: {
+                type: "simple-fill", // 填充符號
+                color: [0, 255, 255, 0.5], // 半透明藍色
+                outline: {
+                    color: [0, 255, 255, 1],
+                    width: 1
+                }
+            }
         }
     });
 
-    // 添加點資料的 GeoJSON 層 (Rush hour.geojson)
-    map.addSource('rush-hour', {
-        type: 'geojson',
-        data: 'https://raw.githubusercontent.com/ryanma20/GeoRyanMa/refs/heads/main/Geojsonfiles/Rush%20hour.geojson'
-    });
-
-    map.addLayer({
-        id: 'rush-hour-layer',
-        type: 'circle',
-        source: 'rush-hour',
-        paint: {
-            'circle-color': 'rgba(255, 0, 0, 0.8)', // 紅色
-            'circle-radius': 8,
-            'circle-stroke-width': 1,
-            'circle-stroke-color': '#ffffff'
+    // 創建點資料的 GeoJSONLayer (Rush hour.geojson)
+    var geojsonLayer2 = new GeoJSONLayer({
+        url: "https://raw.githubusercontent.com/ryanma20/GeoRyanMa/refs/heads/main/Geojsonfiles/Rush%20hour.geojson",
+        renderer: {
+            type: "simple",
+            symbol: {
+                type: "simple-marker", // 簡單點符號
+                style: "circle",
+                color: [255,0, 0, 0.8], // 紅色
+                size: 8,
+                outline: {
+                    color: [255, 255, 255],
+                    width: 1
+                }
+            }
         }
     });
-});
 
-// 點擊事件顯示 Popup
-map.on('click', 'rush-hour-layer', function(e) {
-    var coordinates = e.lngLat;
-    var name = e.features[0].properties.name || "無名稱"; // 確保名稱存在
+    // 設置 PopupTemplate 用來顯示點資料的名稱，標題使用點的 name 欄位
+    geojsonLayer2.popupTemplate = {
+        title: "{name}", // 使用 {name} 顯示每個點的名稱
+        content: function(graphic) {
+            var name = graphic.attributes.name || "無名稱"; // 確保名稱存在
+            console.log("點的屬性:", graphic.attributes); // 檢查屬性
+            return "點名稱: " + name;
+        }
+    };
 
-    new mapboxgl.Popup()
-        .setLngLat(coordinates)
-        .setHTML('點名稱: ' + name)
-        .addTo(map);
-});
+    // 當點擊點資料時顯示名稱
+    view.on("click", function(event) {
+        view.hitTest(event).then(function(response) {
+            if (response.results.length > 0) {
+                var graphic = response.results[0].graphic;
+                if (graphic.layer === geojsonLayer2) {
+                    var name = graphic.attributes.name; // 獲取該點的名稱
 
-// 使點擊圖層外部時彈出視窗關閉
-map.on('mouseleave', 'rush-hour-layer', function() {
-    map.getCanvas().style.cursor = '';
+                    // 顯示 popup
+                    if (name) {
+                        view.popup.open({
+                            title: name, // 使用點的名稱作為標題
+                            content: "點名稱: " + name, // 顯示點名稱
+                            location: event.mapPoint, // 使彈出窗口在點上顯示
+                            dockOptions: {
+                                buttonEnabled: false,
+                                position: "top-right" // 彈出視窗顯示在點的右側
+                            }
+                        });
+                    } else {
+                        view.popup.open({
+                            title: "無名稱", // 如果沒有 "name" 屬性，顯示 "無名稱"
+                            content: "此點沒有名稱", // 顯示提示信息
+                            location: event.mapPoint, // 使彈出窗口在點上顯示
+                            dockOptions: {
+                                buttonEnabled: false,
+                                position: "top-right" // 彈出視窗顯示在點的右側
+                            }
+                        });
+                    }
+                }
+            } else {
+                view.popup.close(); // 如果沒有點擊到任何資料，則關閉彈出視窗
+            }
+        });
+    });
+
+    // 將兩個 GeoJSONLayer 添加到地圖
+    map.addMany([geojsonLayer1, geojsonLayer2]);
+
 });
